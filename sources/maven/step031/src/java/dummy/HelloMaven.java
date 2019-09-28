41c7601cf1d1eea7229f19774b788e40fff36d53
jhLee <kim0lil@naver.com>
2019-09-29 02:35:28 +0900

    20190929

---
+++ sources/maven/step031/src/java/dummy/HelloMaven.java
@@ -0,0 +1,19 @@
+package dummy;
+
+import java.util.Properties;
+
+public class HelloMaven {
+    public static void main(String[] args) throws Exception{
+        Properties props = new Properties();
+
+        props.load(ClassLoader.getSystemClassLoader().getResourceAsStream("configuration.properties"));
+
+        String sName    = (String)props.get("name");
+        String sVersion = (String)props.get("version");
+        String sSkillLv = (String)props.get("skill-lv");
+
+        System.out.println("name : "       + sName
+                          +", version : "  + sVersion
+                          +", skill-lv : " + sSkillLv);
+    }
+}
